ec2f9cacdc2bfd7eb999905bdb3d06bf993673aa
Alexander Zanin <alexandr.zanin@gmail.com>
2022-01-13 11:20:09 +0200

    first commit

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,15 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import "../styles/globals.css";
+import type { AppProps } from "next/app";
+import { IntercomProvider } from "react-use-intercom";
+
+const INTERCOM_APP_ID = "jcabc7e3";
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <IntercomProvider appId={INTERCOM_APP_ID}>
+      <Component {...pageProps} />
+    </IntercomProvider>
+  );
 }
 
-export default MyApp
+export default MyApp;
